7dd8a07889e4a7253dd96a7b19a0a0c9ebd5ed02
Supernova284 <60196464+Supernova284@users.noreply.github.com>
2021-12-02 19:56:21 -0800

    Update Doc_Office.sql

--- mysql/Doc_Office.sql
+++ mysql/Doc_Office.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS Doc_Office;
 CREATE DATABASE Doc_Office;
 DROP TABLE IF EXISTS DOCTOR;
 CREATE TABLE DOCTOR(
-	Doctor_ID int,
+    Doctor_ID int,
     First_name varchar(45),
     Last_name varchar(45),
     Phone_number char(9),
@@ -15,7 +15,7 @@ CREATE TABLE DOCTOR(
     
 DROP TABLE IF EXISTS PATIENT;
 CREATE TABLE PATIENT(
-	SSN char(9),
+    SSN char(9),
     First_name varchar(45),
     Last_name varchar(45),
     Phone_number char(9),
@@ -31,27 +31,27 @@ CREATE TABLE PATIENT(
     
 DROP TABLE IF EXISTS PRESCRIPTION;
 CREATE TABLE PRESCRIPTION(
-	Prescription_ID int,
-	Drug_name varchar(45),
-	Dosage int,
-	Number_of_refills int,
-	Date_prescribed char(10),
-	Most_recent_filling char(10),
-	Prescribed_by int,
-	Patient_SSN char(9),
-	PRIMARY KEY (Prescription_ID),
-	FOREIGN KEY (Date_prescribed) REFERENCES APPOINTMENT(Date),
-	FOREIGN KEY (Prescribed_by) REFERENCES DOCTOR(Doctor_ID),
-	FOREIGN KEY (Patient_SSN) REFERENCES PATIENT(SSN)
+     Prescription_ID int,
+     Drug_name varchar(45),
+     Dosage int,
+     Number_of_refills int,
+     Date_prescribed char(10),
+     Most_recent_filling char(10),
+     Prescribed_by int,
+     Patient_SSN char(9),
+     PRIMARY KEY (Prescription_ID),
+     FOREIGN KEY (Date_prescribed) REFERENCES APPOINTMENT(Date),
+     FOREIGN KEY (Prescribed_by) REFERENCES DOCTOR(Doctor_ID),
+     FOREIGN KEY (Patient_SSN) REFERENCES PATIENT(SSN)
 );
 CREATE INDEX Name
 ON DOCTOR(Doctor_ID,First_name);
 
 DROP TABLE IF EXISTS APPOINTMENT;
 CREATE TABLE APPOINTMENT(
-	Appointment_number int,
+    Appointment_number int,
     Test_given int,
-	Patient_SSN char(9),
+    Patient_SSN char(9),
     Doctor_ID int,
     Time char(5),
     Date char(10),
@@ -66,15 +66,13 @@ ON APPOINTMENT(Date);
 
 DROP TABLE IF EXISTS MEDICAL_TEST;
 CREATE TABLE MEDICAL_TEST(
-	Test_ID int,
-    Doctor_name varchar(45),
+    Test_ID int,
     Doctor_ID int,
     Test_type varchar(45),
     Result varchar(100),
     Patient_SSN char(9),
     Date_given char(10),
     PRIMARY KEY (Test_ID),
-    FOREIGN KEY (Doctor_name) REFERENCES DOCTOR(First_name),
     FOREIGN KEY (Doctor_ID) REFERENCES DOCTOR(Doctor_ID),
     FOREIGN KEY (Patient_SSN) REFERENCES PATIENT(SSN),
     FOREIGN KEY (Date_given) REFERENCES APPOINTMENT(Date)
@@ -84,19 +82,17 @@ ON DOCTOR(Specialty);
 
 DROP TABLE IF EXISTS AUDIT;
 CREATE TABLE AUDIT(
-	Doctor_ID int,
-    Doctor_name varchar(45),
+    Doctor_ID int,
     Action varchar(100),
     Specialty varchar(45),
     Date_modified char(10),
     FOREIGN KEY (Doctor_ID) REFERENCES DOCTOR(Doctor_ID),
-    FOREIGN KEY (Doctor_name) REFERENCES DOCTOR(First_name),
     FOREIGN KEY (Specialty) REFERENCES DOCTOR(Specialty)
     );
     
 DROP TABLE IF EXISTS ACCOUNT;
 CREATE TABLE ACCOUNT(
-	username varchar(45),
+    username varchar(45),
     role varchar(45),
     hash varchar(45),
     salt varchar(45)
