b3b7c9a2a4b4536cd6b44c559684eeaf48296013
Supernova284 <60196464+Supernova284@users.noreply.github.com>
2021-12-09 09:23:59 -0800

    Update Doc_Office.sql

--- mysql/Doc_Office.sql
+++ mysql/Doc_Office.sql
@@ -10,78 +10,166 @@ CREATE TABLE ACCOUNT(
     hash varchar(100)
     );
 CREATE TABLE DOCTOR(
-	  Doctor_ID int NOT NULL UNIQUE AUTO_INCREMENT,
-    First_name varchar(45) ,
+	Doctor_ID int NOT NULL AUTO_INCREMENT,
+    First_name varchar(45),
     Last_name varchar(45),
-    Specialty varchar(45),
-    Salary int,
     username varchar(45),
+    Salary int,
     Phone_number char(10),
-    FOREIGN KEY (username) REFERENCES ACCOUNT(username),
-    Primary Key (Doctor_ID)
+    PRIMARY KEY (Doctor_ID),
+    FOREIGN KEY (username) REFERENCES ACCOUNT(username)
     );
+    
+DROP TABLE IF EXISTS APPOINTMENT;
+CREATE TABLE APPOINTMENT(
+	Appointment_number int AUTO_INCREMENT,
+    date datetime,
+    doctorid int,
+    roomNumber int,
+    PRIMARY KEY (Appointment_number),
+    FOREIGN KEY(doctorid) REFERENCES DOCTOR(Doctor_ID)
+    );
+    
 DROP TABLE IF EXISTS PATIENT;
 CREATE TABLE PATIENT(
-	  SSN char(9) UNIQUE,
+	ssn char(9),
+	patientid int NOT NULL,
     First_name varchar(45),
     Last_name varchar(45),
     Street_name varchar(50),
-    Street_number char(10),
+    Stree_number varchar(10),
     City varchar(45),
     Zip_code varchar(10),
-    Insurance_ID int,
-    username varchar(45),
     Phone_number char(10),
-    FOREIGN KEY (username) REFERENCES ACCOUNT(username),
-    Primary Key (SSN)
+    Appointment_number int,
+    username varchar(45),
+    PRIMARY KEY(patientid),
+    FOREIGN KEY (Appointment_number) REFERENCES APPOINTMENT(Appointment_number),
+    FOREIGN KEY (username) REFERENCES ACCOUNT(username)
     );
     
 DROP TABLE IF EXISTS PRESCRIPTION;
 CREATE TABLE PRESCRIPTION(
 	Prescription_ID int,
-	Drug_name varchar(45),
-	Dosage int,
-	Number_of_refills int,
-	Prescribed_by int AUTO_INCREMENT,
-	Patient_SSN char(9),
+    medicineName varchar(100),
+    doctorid int,
+    Dosage int,
+    Numnber_of_refills int,
     Date_prescribed datetime,
     Most_recent_filling datetime,
-	PRIMARY KEY (Prescription_ID),
-	FOREIGN KEY (Prescribed_by) REFERENCES DOCTOR(Doctor_ID),
-	FOREIGN KEY (Patient_SSN) REFERENCES PATIENT(SSN)
+    PRIMARY KEY(Prescription_ID),
+    FOREIGN KEY(doctorid) REFERENCES DOCTOR(Doctor_ID)
 );
 
-DROP TABLE IF EXISTS MEDICAL_TEST;
-CREATE TABLE MEDICAL_TEST(
-	  Test_ID int AUTO_INCREMENT,
-    Doctor_ID int,
-    Test_type varchar(45),
-    Result varchar(100),
-    Patient_SSN char(9),
+DROP TABLE IF EXISTS TEST;
+CREATE TABLE TEST(
+	testname varchar(100),
+    result varchar(100),
+    Prescription_ID int NOT NULL,
     Date_given datetime,
-    PRIMARY KEY (Test_ID),
-    FOREIGN KEY (Doctor_ID) REFERENCES DOCTOR(Doctor_ID),
-    FOREIGN KEY (Patient_SSN) REFERENCES PATIENT(SSN)
+    PRIMARY KEY (testname),
+    FOREIGN KEY(Prescription_ID) REFERENCES PRESCRIPTION(Prescription_ID)
 );
 
-DROP TABLE IF EXISTS APPOINTMENT;
-CREATE TABLE APPOINTMENT(
-	Appointment_number int,
-    Test_given int,
-	Patient_SSN char(9),
-    Doctor_ID int AUTO_INCREMENT,
-    Date datetime,
-    Room_number int,
-    PRIMARY KEY (Appointment_number),
-    FOREIGN KEY (Patient_SSN) REFERENCES PATIENT(SSN),
-    FOREIGN KEY (Test_given) REFERENCES MEDICAL_TEST(Test_ID),
+DROP TABLE IF EXISTS SPECIALIZATION;
+CREATE TABLE SPECIALIZATION(
+	specialtyid int,
+    specialty varchar(45),
+    PRIMARY KEY (specialtyid)
+    );
+DROP TABLE IF EXISTS AUDIT;
+CREATE TABLE AUDIT(
+	id int AUTO_INCREMENT,
+	Doctor_ID int,
+    Action varchar(100),
+    Date_modified datetime,
+    PRIMARY KEY (id),
     FOREIGN KEY (Doctor_ID) REFERENCES DOCTOR(Doctor_ID)
     );
 
-DROP TABLE IF EXISTS AUDIT;
-CREATE TABLE AUDIT(
-	Doctor_ID int AUTO_INCREMENT,
-    Action varchar(100),
-    Date_modified datetime,
-    FOREIGN KEY (Doctor_ID) REFERENCES DOCTOR(Doctor_ID)
+DROP TABLE IF EXISTS DOCTORPATIENT;
+CREATE TABLE DOCTORPATIENT (
+  doctorId INT NOT NULL,
+  patientid int NOT NULL,
+  PRIMARY KEY (doctorId, patientid),
+  CONSTRAINT doctorid
+    FOREIGN KEY (doctorId)
+    REFERENCES DOCTOR (Doctor_ID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT patientid
+    FOREIGN KEY (patientid)
+    REFERENCES PATIENT (patientid)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+DROP TABLE IF EXISTS DOCTORTEST;
+CREATE TABLE DOCTORTEST(
+	doctorId int NOT NULL,
+    testName varchar(45) NOT NULL,
+    PRIMARY KEY (doctorId, testName),
+    CONSTRAINT doctor_id
+	  FOREIGN KEY (doctorId) REFERENCES DOCTOR(Doctor_ID)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+	CONSTRAINT testName
+      FOREIGN KEY (testName) REFERENCES TEST(testname)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+    );
+
+DROP TABLE IF EXISTS DOCTORSPECIALIZATION;
+CREATE TABLE  DOCTORSPECIALIZATION(
+	doctorId int NOT NULL,
+    specialtyid int NOT NULL,
+    PRIMARY KEY (doctorId, specialtyid),
+    CONSTRAINT doctor
+	  FOREIGN KEY (doctorId) REFERENCES DOCTOR(Doctor_ID)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+	CONSTRAINT specialtyid
+      FOREIGN KEY (specialtyid) REFERENCES SPECIALIZATION(specialtyid)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+    );
+DROP TABLE IF EXISTS TESTAPPOINTMENT;
+CREATE TABLE  TESTAPPOINTMENT(
+	testname varchar(100) NOT NULL,
+    appointmentid int NOT NULL,
+    PRIMARY KEY (testname, appointmentid),
+    CONSTRAINT testname_
+	  FOREIGN KEY (testname) REFERENCES TEST(testname)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+	CONSTRAINT appointmentid_
+      FOREIGN KEY (appointmentid) REFERENCES APPOINTMENT(Appointment_number)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+    );
+DROP TABLE IF EXISTS TESTPATIENT;
+CREATE TABLE  TESTPATIENT(
+	testname varchar(100) NOT NULL,
+	patientid int NOT NULL,
+    PRIMARY KEY (testname, patientid),
+    CONSTRAINT test_name
+	  FOREIGN KEY (testname) REFERENCES TEST(testname)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+	CONSTRAINT patientid_
+      FOREIGN KEY (patientid) REFERENCES PATIENT(patientid)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+    );
+CREATE TABLE PRESCRIPTIONPATIENT(
+	Prescription_ID int NOT NULL,
+	patientid int NOT NULL,
+    PRIMARY KEY (Prescription_ID, patientid),
+    CONSTRAINT Prescription_ID 
+	  FOREIGN KEY (Prescription_ID ) REFERENCES PRESCRIPTION(Prescription_ID )
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+	CONSTRAINT patient
+      FOREIGN KEY (patientid) REFERENCES PATIENT(patientid)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
     );
